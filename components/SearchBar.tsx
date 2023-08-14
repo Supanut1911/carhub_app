9f7563d93136def5a8fddb94b5db760021d1632f
Supanut1911 <ynotmela@gmail.com>
2023-08-14 17:18:53 +0700

    feat: implement combox_headlessui at searchManufacturer

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -10,7 +10,10 @@ const SearchBar = () => {
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__items">
-        <SearchManufacturer manufacturer="" setManufacturer={setManufacturer} />
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
       </div>
     </form>
   );
