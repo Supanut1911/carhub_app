bb69f035e56caf3d551f981207776ea6b62e1b4d
Supanut1911 <ynotmela@gmail.com>
2023-08-15 00:06:38 +0700

    feat: ✨cardDetail component and dialo

--- components/CardDetails.tsx
+++ components/CardDetails.tsx
@@ -1,5 +1,11 @@
+"use client";
+
 import { CarProps } from "@/types";
-import React from "react";
+import Image from "next/image";
+
+import { Fragment } from "react";
+
+import { Dialog, Transition } from "@headlessui/react";
 
 interface CarDetailProp {
   isOpen: boolean;
@@ -8,7 +14,71 @@ interface CarDetailProp {
 }
 
 const CardDetails = ({ isOpen, closeModal, car }: CarDetailProp) => {
-  return <div>CardDetails</div>;
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className={`relative z-10 bg-500-red w-10 h-10`}
+          onClose={closeModal}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel
+                  className={`relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left
+                    shadow-xsl transition-all flex flex-col gap-5
+                  `}
+                >
+                  <button
+                    type="button"
+                    onClick={closeModal}
+                    className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                  >
+                    <Image
+                      src="/close.svg"
+                      width={20}
+                      height={20}
+                      alt="close"
+                      className="object-contain"
+                    />
+                  </button>
+                  <div className="flex-1 flex flex-col gap-3">
+                    <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
+                      <Image
+                        src="/hero.png"
+                        alt="car-hero"
+                        fill
+                        priority
+                        className="object-contain"
+                      />
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
 };
 
 export default CardDetails;
